df4fce2860986d06b645e17fd3eed31110a19b58
Jing Liu <jingliuweb@gmail.com>
2019-08-21 11:43:35 +0200

    reverse notes data

--- src/components/notes/NoteList.js
+++ src/components/notes/NoteList.js
@@ -5,7 +5,7 @@ const NoteList = ({notes}) => {
     return (
         <div className="tile is-8 is-vertical is-info">
             {
-                notes.length > 0 ? notes.map(note => (
+                notes.length > 0 ? notes.slice(0).reverse().map(note => (
                     <NoteSummary key={note.id} note={note} />
                 ))
                 : <div className="notification is-success">There is not any note here. Add one now!</div>
